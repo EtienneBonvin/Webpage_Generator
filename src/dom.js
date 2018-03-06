b363d43b1358cc1b7274e652b562663f01c3700f
Etienne Bonvin <etienne.bonvin@epfl.ch>
2018-03-06 14:10:39 +0100

    Simple navigation adding

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,19 @@
-
+/**
+* Create a clickable element with a given type.
+*
+* @param String elementType : the type of the element to create.
+* @param String textToDisplay : the text to display on the element.
+* @param function onClick : the function to execute when clicking on the created element.
+*
+* @return HTML Element : an element with the given attributes.
+*/
+function clickableElement(elementType, textToDisplay, onClick){
+    var element = document.createElement(elementType);
+    element.innerHTML = textToDisplay;
+    element.addEventListener("click", onClick);
+    element.style.cursor = "pointer";
+    return element;
+}
 
 /**
 * Create a division with the given with (in percent of the screen's width).
