10422e5d6ead28df26c7de5d6aeb5c96d2e54a52
Etienne Bonvin <etienne.bonvin@epfl.ch>
2018-03-09 10:46:28 +0100

    Comment code

--- src/generator.js
+++ src/generator.js
@@ -1,5 +1,12 @@
+/**
+* Core logic of the generator.
+* This document contains the WebPage class as well as helper classes and functions.
+*/
 
 
+/**
+* Class WebPage.
+*/
 function WebPage(){
     this.banner = centeredDivWithId("100%", "banner");
     this.navigation = centeredDivWithId("100%", "navigation");
@@ -16,6 +23,13 @@ function WebPage(){
     $("body").append(separation());
     $("body").append(this.footer);
     
+    
+    /**
+    * Sets the banner with an image in the upper left corner and an application name on the upper right.
+    *
+    * @param String src : the path of the image to display.
+    * @param String name : the name of the application to display.
+    */
     this.setBanner = function(src, name){
         $("banner").empty();
         var imageDiv = sizedDiv("40%");
@@ -27,6 +41,12 @@ function WebPage(){
         parenting(this.banner, pageName);
     }
     
+    
+    /**
+    * Sets a colored headband for the webpage under the banner. Usefull as decoration when no navigation is set.
+    *
+    * @param String color : the hex code or name of the color of the headband.
+    */
     this.setHeadband = function(color){
         $("headband").empty();
         var head = sizedDiv("100%");
@@ -34,14 +54,33 @@ function WebPage(){
         parenting(this.headband, head);
     }
     
+    
+    /**
+    * Sets the content of the webpage.
+    *
+    * @param HTMLObject content : the HTML object representing the content of the webpage.
+    */
     this.addContent = function(content){
         parenting(this.content, content);
     }
     
+    
+    /**
+    * Sets an informations footer at the bottom of the web page.
+    *
+    * @param String message : the message to display in the footer.
+    */
     this.setFooter = function(message){
         footer.append(message);
     }
     
+    
+    /**
+    * Sets a navigation bar for the webpage.
+    *
+    * @param String color : the color of the navigation bar.
+    * @param ...Tab tabs : the tabs of the navigation bar.
+    */
     this.setNavigation = function(color, ...tabs){
         var nav = sizedDiv("100%");
         for(var i=0; i < tabs.length; i++){
@@ -53,18 +92,40 @@ function WebPage(){
     }
 }
 
+
+/**
+* Tab helper class.
+* Represents a tab of the navigation bar, with a name and an onClick function.
+*
+* @param String name : the name of the tab.
+* @param Function onClick : the function to execute on click.
+*/
 function Tab(name, onClick){
     this.name = name;
     this.onClick = onClick;
     
+    
+    /**
+    * Unused for now.
+    * Returns the on click function of the tab.
+    */
     this.changeTab = function(){
         this.onClick();
     }
     
+    
+    /**
+    * Usused for now.
+    * Returns the name of the tab.
+    */
     this.getName = function(){
         return this.name;
     }
     
+    
+    /**
+    * Creates a tab as a clickable HTML element with the "tab" css' class name.
+    */
     this.createTab = function(){
         var element = clickableElement("div", this.name, function(){
             onClick();
--- src/test.js
+++ src/test.js
@@ -1,18 +1,41 @@
+/**
+*
+* Test document.
+* This document shows and describe how the Webpage Generator should be used.
+*
+*/
+
 var page;
 
 $('document').ready(function(){
+    
+    /* Creates a new Webpage skeleton */
     page = new WebPage();
+    
+    /* Adds a banner and an application name to the page */
     page.setBanner("res/pics/EPFL-Logo.jpg", "Test application");
+    
+    /* Defines the navigation bar, here only two tabs */
     page.setNavigation("red", new Tab("Tab1", function(){showTab1()}), new Tab("Tab2", function(){showTab2()}));
+    
+    /* If no navigation is used, one can add a colored headband to separate banner and content */
     //page.setHeadband("red");
+    
+    /* Sets the actual content of the page */
     page.addContent(paragraph("Hello world !"));
+    
+    /* Set the footer of the page */
     page.setFooter("Test application. All rights reserved.");
 });
 
+
+/* Function called when the first tab is clicked */
 function showTab1(){
     page.addContent(paragraph("Switch to tab 1 !"));
 }
 
+
+/* Function called when the second tab is clicked */
 function showTab2(){
     page.addContent(paragraph("Switch to tab 2 !"));
 }
